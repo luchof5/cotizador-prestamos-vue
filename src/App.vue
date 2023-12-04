6b8e6ce1d4a2645a78b29d6a2e6e0cd8705b0196
Luis Furtado <lucho_l.f@hotmail.com>
2023-12-04 19:25:27 -0300

    type range

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <script setup>
+  import { ref } from 'vue'
+  import Header from './components/Header.vue'
+  
+  const cantidad = ref(0);
+
+  function handleChange(e) {
+    cantidad.value = +e.target.value
+  }
 
 </script>
 
 <template>
-  <h1 class="text-4xl font-black">Hola Mundo</h1>
+  <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+    <Header />
+
+    <div class="my-5">
+      <input 
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        @input="handleChange"
+      />
+
+      {{ cantidad }}
+    </div>
+  </div>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1 class="text-4xl font-extrabold text-gray-500 text-center">
+            ¿Cuánto <span class="text-indigo-600">dinero </span> necesitas?
+        </h1>
+    </div>
+</template>
