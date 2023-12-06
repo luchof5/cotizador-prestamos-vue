5510442f13a246ad2b1a5911a9e9e28eb135d29e
Luis Furtado <lucho_l.f@hotmail.com>
2023-12-06 15:17:32 -0300

    botones agregados

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import { ref, computed } from 'vue'
   import Header from './components/Header.vue'
+  import Button from './components/Button.vue'
   
   const cantidad = ref(10000);
   const MIN = 0;
@@ -15,11 +16,42 @@
     return formatter.format(cantidad.value);
   });
 
+  const handleChangeDecremento = () => {
+    const valor = cantidad.value - STEP;
+    if (valor < MIN) {
+      alert('Cantidad no válida');
+      return;
+    }
+    cantidad.value = valor;
+  }
+
+  const handleChangeIncremento = () => {
+    const valor = cantidad.value + STEP;
+    if (valor > MAX) {
+      alert('Cantidad no válida');
+      return;
+    }
+    cantidad.value = valor;
+  }
+
 </script>
 
 <template>
   <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
+
+    <div class="flex justify-between mt-10">
+      
+      <Button 
+        :operador="'-'"
+        @fn="handleChangeDecremento"
+      />
+      <Button 
+        :operador="'+'"
+        @fn="handleChangeIncremento"
+      />
+
+    </div>
 
     <div class="my-5">
       <input 
